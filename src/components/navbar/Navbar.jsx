c3de65fe077285e924f84744c9bbf6123ff431a8
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-21 10:38:53 +0100

    Add node-sass

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./navbar.scss"
+
+export default function Navbar() {
+  return (
+    <div className="navigation_bar_stars_friends">
+        <h1>Hello</h1>
+    </div>
+  )
+}
