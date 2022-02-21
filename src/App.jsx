4a5c34f3c9064f3a4709365cd3c2be9a6d2ea7d3
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-21 15:47:03 +0100

    App and images json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
 import "./app.scss"
+import Roadmap from "./components/roadmap/Roadmap";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div className="container">
         <Navbar />
         <Hero />
+        <About />
+        <Roadmap />
       </div>
     </div>
   );
