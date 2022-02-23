bd8074373cd4ecc069e09916065850316adbf3c8
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-02-23 17:06:24 +0100

    Update All Components

--- src/components/discord/Discord.jsx
+++ src/components/discord/Discord.jsx
@@ -5,18 +5,20 @@ export default function Discord() {
   return (
     <div className="discord">
       <img className="discord_bg_top" src="assets/cloud_top.png" alt="" />
-      <div className="container">
-        <h1>Join our community</h1>
-        <div className="discord_bg">
-          <p>
-            Stars Friends Discord just opened on the galaxy ! If you want to
-            join the #starsfriends it’s here. Join us to get the news as soon as
-            possible and follow our latest announcements.
-          </p>
-          <div className="btn_discord">
-            <button href="discord.gg/ZgbavxQB7J" target="_blank">
-              Join Our discord
-            </button>
+      <div className="background_global">
+        <div className="container">
+          <h1>Join our community</h1>
+          <div className="discord_bg">
+            <p>
+              Stars Friends Discord just opened on the galaxy ! If you want to
+              join the #starsfriends it’s here. Join us to get the news as soon
+              as possible and follow our latest announcements.
+            </p>
+            <div className="btn_discord">
+              <button href="discord.gg/ZgbavxQB7J" target="_blank">
+                Join Our discord
+              </button>
+            </div>
           </div>
         </div>
       </div>
